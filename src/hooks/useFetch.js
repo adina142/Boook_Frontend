479b8a08569ac0e7ceb35d6f60b6af727c206957
adina142 <adinakhalid99@gmail.com>
2025-10-19 22:45:11 +0500

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://book-backend1-1.onrender.com";;
 
 export const useFetch = (endpoint) => {
   const [data, setData] = useState([]);
