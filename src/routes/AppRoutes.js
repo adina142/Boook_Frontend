622b477bad60e7b8a31cbbf0f43f7baf33149df2
adina142 <adinakhalid99@gmail.com>
2025-10-19 20:53:38 +0500

    Enhance routing with process and standards features
    Added new routes for process management and standards.

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -6,7 +6,9 @@ import Login from "../pages/Login/Login";
 import Standards from "../pages/Standards/Standards";
 import Comparisons from "../pages/Comparisons/Comparisons";
 import Process from "../pages/Process/Process";
-
+import ProcessBuilder from "../pages/ProcessBuilder/ProcessBuilder";
+import ProcessTemplates from "../pages/ProcessTemplates/ProcessTemplates";
+import ProcessRecommendations from "../pages/ProcessRecommendations/ProcessRecommendations";
 
 const AppRoutes = () => {
   return (
@@ -17,13 +19,32 @@ const AppRoutes = () => {
 
       {/* Standard feature pages */}
       <Route path="/standards" element={<Standards />} />
+      <Route path="/standards/:standardSlug" element={<Standards />} />
+      <Route path="/standards/:standardSlug/section/:sectionId" element={<Standards />} />
+      
       <Route path="/comparisons" element={<Comparisons />} />
-      <Route path="/process" element={<Process />} />
+      
+      {/* Process routes */}
+      <Route path="/process" element={<Process />}>
+        <Route index element={<Navigate to="builder" replace />} />
+        <Route path="builder" element={<ProcessBuilder />} />
+        <Route path="templates" element={<ProcessTemplates />} />
+        <Route path="recommendations" element={<ProcessRecommendations />} />
+      </Route>
 
-     
+      {/* Alternative approach: If you prefer nested routes without layout */}
+      {/* 
+      <Route path="/process" element={<Process />} />
+      <Route path="/process/builder" element={<ProcessBuilder />} />
+      <Route path="/process/templates" element={<ProcessTemplates />} />
+      <Route path="/process/recommendations" element={<ProcessRecommendations />} />
+      */}
 
       {/* Redirect from singular to plural */}
       <Route path="/comparison" element={<Navigate to="/comparisons" replace />} />
+      
+      {/* Catch all route - redirect to home */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
